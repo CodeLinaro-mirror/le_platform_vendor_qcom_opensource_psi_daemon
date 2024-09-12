CodeLinaro-mirror/le_platform_vendor_qcom_opensource_psi_daemon

commit 88c84670446eb488e2855628bd1052ff0c7ac008

psi_daemon: Do not count anon memory if swap is full or disabled
When computing the amount of reclaimable system memory, anonymous
pages should only be considered if they can be swapped out. Additionally,
as zram is in use, the amount of memory reclaimed by swapping out one
page is less than the full page. Estimate the compression ratio as 50%.

Based off prior work by Charan Teja Kalla <quic_charante@quicinc.com>.

Change-Id: I4b3d9c6baa2d43c8f1e22767b71f056f74f5d197
Signed-off-by: Charan Teja Kalla <quic_charante@quicinc.com>
Signed-off-by: Patrick Daly <quic_pdaly@quicinc.com>

psi_daemon.cpp
@@ -115,6 +115,7 @@ struct memory_snapshot {
     uint64_t movable_free_kb;
     uint64_t movable_inactive_anon_kb;
     uint64_t movable_inactive_file_kb;
+    uint64_t swap_free_kb;
 };
 
 struct psi_pressure {
@@ -180,6 +181,11 @@ std::atomic<bool> wait_in_progress(false);
 
 /* stall tracking window size, 50ms*/
 static int PSI_WINDOW_SIZE_US = (50 * US_PER_MS);
+/*
+ * The percent reduction in size of anonymous memory after being
+ * swapped out and compressed by zram.
+ */
+static const int ZRAM_COMPRESSION_RATIO_PCT = 50;
 
 #define TARGET_OOM_SCORE_ADJ    -1000
 
@@ -389,6 +395,8 @@ static int system_memory_snapshot(struct memory_snapshot *mem_snap)
 
     if(parse_field(buf, "MemFree", &mem_snap->sys_memfree_kb))
         goto err;
+    if(parse_field(buf, "SwapFree", &mem_snap->swap_free_kb))
+	    goto err;
     res = 0;
 
 err:
@@ -707,6 +715,31 @@ int __weak get_kernel_plugin_count(size_t __unused *count) {
     return -ENOTTY;
 }
 
+static uint64_t get_memsnap_and_total_free(struct memory_snapshot *mem_snap)
+{
+    uint64_t free;
+
+    if (system_memory_snapshot(mem_snap))
+        return 0;
+
+   /*
+    * inactive_file pages can be reclaimed easily, and
+    * inactive_anon pages can be swapped and reused.
+    *
+    * Discount inactive anon by a factor of 2 assuming a compression ratio
+    * of 50% with zram.
+    *
+    * Free pages in the normal zone are ignored; lowmem_reserve is assumed
+    * to prohibit fallback from movable to normal zone.
+    */
+    free = mem_snap->movable_free_kb +
+	    mem_snap->movable_inactive_file_kb;
+    free += std::min(mem_snap->swap_free_kb, mem_snap->movable_inactive_anon_kb) *
+            ZRAM_COMPRESSION_RATIO_PCT / 100;
+
+    return free;
+}
+
 void* memtrack_thread_function(void *arg) {
 
     (void)(arg);
@@ -763,7 +796,8 @@ startover:
         //TODO: add more checks for memory stats such as reclaimable memory, zram etc.
 
         /* take snapshot of memory */
-        if (system_memory_snapshot(&mem_snap))
+        total_free = get_memsnap_and_total_free(&mem_snap);
+        if (!total_free)
             continue;
 
         LOG(INFO) << "MemFree before UNPLUG: " << mem_snap.sys_memfree_kb <<
@@ -772,15 +806,6 @@ startover:
 
         LOG(INFO) << "Movable inactive_file: " << mem_snap.movable_inactive_file_kb <<
                         " KB : inactive_anon: " << mem_snap.movable_inactive_anon_kb << " KB";
-
-        total_free = mem_snap.movable_free_kb;
-
-        /*
-         * inactive_file pages can be reclaimed easily, and
-         * inactive_anon pages can be swapped and reused.
-         */
-        total_free += mem_snap.movable_inactive_file_kb +
-                mem_snap.movable_inactive_anon_kb;
 
         count = (total_free / SIZE_1KB) / resolution;
 

memory_plugin_interface.cpp
@@ -160,6 +160,9 @@ int get_kernel_plugin_count(size_t *count) {
 int memory_unplug_request_kernel(size_t count) {
     char str_val[LINE_MAX];
 
+    if (!count)
+	    return 0;
+
     snprintf(str_val, sizeof(str_val), "%lu", count);
     if (write_file(QVM_NUM_KERNEL_UNPLUG_PATH, str_val)) {
         LOG(ERROR) << "Failed to write to " << QVM_NUM_KERNEL_UNPLUG_PATH;

commit 6cfb8c78cba1f692a1ee6e1144d5e9e96476d263

psi_daemon: fix longer holding of wake locks
Fix the possibility of holding the wake locks for longer durations.

Change-Id: I14daf8d277596f8b745caadf4698d5ef400563ca
Fixes: 88c84670446e("psi_daemon: Do not count anon memory if swap is full or disabled")
Signed-off-by: Charan Teja Kalla <quic_charante@quicinc.com>

psi_daemon.cpp
@@ -798,7 +798,7 @@ startover:
         /* take snapshot of memory */
         total_free = get_memsnap_and_total_free(&mem_snap);
         if (!total_free)
-            continue;
+            goto retry;
 
         LOG(INFO) << "MemFree before UNPLUG: " << mem_snap.sys_memfree_kb <<
                 " KB (Normal: " << mem_snap.normal_free_kb << " KB, Movable: " <<
@@ -856,12 +856,13 @@ release_blocks:
 
         /* take memory snapshot after unplugging */
         if (system_memory_snapshot(&mem_snap))
-            continue;
+            goto retry;
 
         LOG(DEBUG) << "MemFree after UNPLUG: " << mem_snap.sys_memfree_kb <<
                 " KB (Normal: " << mem_snap.normal_free_kb << " KB, Movable: " <<
                 mem_snap.movable_free_kb<< " KB)";
 
+retry:
         if (mem_chunks_plugged && retry_count < MAX_UNPLUG_RETRY) {
             ++retry_count;
             LOG(INFO) << "Retrying unplug after " << IDLE_WAIT_TIME_S <<
@@ -869,6 +870,8 @@ release_blocks:
             goto startover;
         }
         else {
+	    if (!total_free)
+		LOG(INFO) << "No free memory left to unplug";
             if (retry_count == MAX_UNPLUG_RETRY)
                 LOG(INFO) << "Max retry attempt reached for unplugging!!";
             if (!mem_chunks_plugged)
@@ -877,7 +880,6 @@ release_blocks:
         }
 
         //TODO: should we keep checking until all plugged memory is unplugged? goto startover ?
-
         if(wake_locked && !wake_unlock())
             LOG (ERROR) << "failed to wake unlock";
         else